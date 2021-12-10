ed34e77799c2c6446bd4d236c57d2af3d02ca2f6
amrit <amritthap966@gmail.com>
2021-12-10 15:21:43 +0530

    adding data from api

--- js/script.js
+++ js/script.js
@@ -2,48 +2,61 @@ const timeEl= document.getElementById('time');
 const monthEl= document.getElementById('month');
 const weekdayEl= document.getElementById('weekday');
 const tempEl= document.getElementById('temp');
-const countryEl= document.getElementsByClassName('country-zone');
+const countryEl= document.getElementById('country-zone');
+const fiveDayElement= document.getElementById("fiveday");
 
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-const API_KEY ='4caff178f82066088df8989f85d0240e';
+const API_KEY = '4caff178f82066088df8989f85d0240e';
 
-setInterval(()=>{
-    const time =new Date();
-    const month= time.getMonth();
-    const date=time.getDate();
-    const day=time.getDay();
-    const hour=time.getHours();
-    const otherformat= hour >= 13 ? hour %12: hour
-    const munite = time.getMinutes();
-    const ampm= hour >= 12?  'PM' : 'AM'
+const getWeatherData = async () => {
+    const res = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=be54106484a670d36ecd3ebbd14ed34e`)
+    const data = await res.json();
+    showWeatherData(data);
+};
 
-   
-    timeEl.innerHTML = (otherformat < 10? '0'+otherformat : otherformat) + ': ' + (munite < 10? '0'+munite: munite)+ ' ' + `<span id="am-pm">${ampm}</span>`
+const showWeatherData = (data) => {
+    const currentTime = ((new Date().getHours()) / 3) - 4;
+    countryEl.innerText = `${data.city.name}, ${data.city.country}`;    
+    let daysAndData = [];
+    for (let i = 0; i < data.list.length ; i += 8) {
+        temp = data.list.slice(i, i + 8);
+        daysAndData.push(temp)
+    }
+    daysAndData.forEach((dayData, index) => {
+        console.log(dayData)
+        const day = document.createElement("div");
+        day.setAttribute("class", "day")
+        day.innerHTML = `<h4 class="weekday"> ${days[new Date(dayData[0].dt_txt).getDay()]}</h4>`;
 
-    monthEl.innerHTML =  date+ ' ' + months[month] +', '
-    weekdayEl.innerHTML = `<h4 id=weekday>${days[day]}</h4>`
-    
-},1000)
+        const dateAndTime = document.createElement("div");
 
-    getWeatherData()
-function getWeatherData () {
+        const month = document.createElement("span");
+        month.innerText = months[new Date(dayData[0].dt_txt).getMonth()];
+        const date = document.createElement("span");
+        date.innerText = new Date(dayData[0].dt_txt).getDate();
+        const time = document.createElement("span");
+        time.innerText = `${new Date(dayData[0].dt_txt).getHours()}: ${(new Date(dayData[0].dt_txt).getMinutes()) >= 10 ? new Date(dayData[0].dt_txt).getMinutes() : "0" + new Date(dayData[0].dt_txt).getMinutes() }`
+        dateAndTime.appendChild(month);
+        dateAndTime.appendChild(date);
+        dateAndTime.appendChild(time);
+        
+        const temp = document.createElement("h3");
+        temp.setAttribute("class", "temp");
+        temp.innerHTML = `${parseFloat((dayData[Math.floor(currentTime)].main.temp) - 273.15).toFixed(2)}℃`;
+        const description = document.createElement("div");
+        description.setAttribute("class", "status");
+        description.innerText = dayData[Math.floor(currentTime)].weather[0].description;
+        const icon = document.createElement("img");
+        icon.setAttribute("src", `http://openweathermap.org/img/wn/${dayData[Math.floor(currentTime)].weather[0].icon}@2x.png`);
 
-    navigator.geolocation.getCurrentPosition((success) => {
-    
-    
-        fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=be54106484a670d36ecd3ebbd14ed34e`).then(res => res.json()).then(data => {
-
-        console.log(data)
-        showWeatherData(data);
-        })
-
+        day.appendChild(dateAndTime);
+        day.appendChild(icon);
+        day.appendChild(temp);
+        day.appendChild(description);
+        
+        fiveDayElement.appendChild(day);
     })
 }
-    
 
-
-    
-    function showWeatherData () {
-
-    }
+getWeatherData();
